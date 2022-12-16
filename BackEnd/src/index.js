981bb9ad99571822444b88fe1c324a835a98d47d
sampaixd <samuelbroman03@gmail.com>
2022-12-16 14:31:36 +0100

    feat added basic groundwork, folder and files

---
+++ BackEnd/src/index.js
@@ -0,0 +1,15 @@
+//////////////  imports    //////////////
+import express from "express";
+import cors from "cors";
+/////////////////////////////////////////
+
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+})
