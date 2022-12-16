a5891406355f4032c44ddf78912034cd54676839
Markus Piotrowski <Markuspiotrowski@live.com>
2022-12-16 13:31:48 +0100

    Innit

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <h1>Hello World</h1>
+  </header>
+</template>
+
+<style scoped></style>
